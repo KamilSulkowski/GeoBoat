3d9b7c6d695800c00b4907a1851dc09fbebe8d66
Varisch <bartek0sobanski@gmail.com>
2023-07-13 21:52:35 +0200

    Poruszanie łodzi
    Poruszanie łodzi:
    +Rozpędzanie z czasem
    +Połączenie wektora z kierunkiem łódki
    +Hamowanie z czasem
    
    Podmiana sprite'a łódki

--- public/src/Main.js
+++ public/src/Main.js
@@ -4,8 +4,8 @@ import Game from './game.js';
 export default new Phaser.Game({
     type: Phaser.AUTO,
     parent: 'app',
-    width: 800, //width mapy - 8000
-    height: 524,//height mapy - 5248
+    width: 1000, //width mapy - 8000
+    height: 800,//height mapy - 5248
     physics: {
         default: 'arcade',
         arcade: {
--- public/src/game.js
+++ public/src/game.js
@@ -2,48 +2,101 @@
 export default class Game extends Phaser.Scene {
     constructor() {
         super('game');
+        this.boatSpeed = 0; //Zmienna do ustawiania prędkości łódki
+        this.timer = 0;     //Zmienna do przeliczania czasu (używana przy łodzi atm)
+        this.engine = 0;    //Zmienna do sprawdzania stanu rozpędu/hamowania łodzi
     }
     Preload(){
 
     }
+
     create(){
         // Ustawienie tła na niebieskie
         this.cameras.main.setBackgroundColor('#87CEEB');
+        const bw = this.cameras.main.width; // width main kamery
+        const bh = this.cameras.main.height;// height main kamery
+
         // ustawia wielkość kamery
-        this.cameras.main.setBounds(0,0, 800,600);
+        this.cameras.main.setBounds(0, 0, 800, 600);
 
         // Ustawienie łódki na środek ekranu
-        this.boat = this.physics.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, "boat");
-        this.boat2 = this.physics.add.sprite((this.cameras.main.width / 2) + 200, (this.cameras.main.height / 2) + 200, "boat");
+        this.boat = this.physics.add.sprite(bw * 0.5, bh * 0.5, "boat");
+        
+        // Druga łódka do testów
+        this.boat2 = this.physics.add.sprite((bw * 0.5) + 200, (bh * 0.5) + 200, "boat");
+    
         // Zmienna do ustawienia sterowania
         this.keys = this.input.keyboard.createCursorKeys();
-
     }
 
     update(time, delta) {
         super.update(time, delta);
+        this.timer += delta;
 
-        this.moveBoat(this.boat, 10);
+        // Poruszanie łodzią
+        this.moveBoat(this.timer);
+        this.boatEngine(this.engine, this.timer);
+
         // kamera śledzi gracza
         this.cameras.main.startFollow(this.boat);
 
+        // Koordynaty środka kamery
+        //console.log(this.cameras.main.midPoint)
     }
 
     // Funkcja do sterowania łodzią
     moveBoat(){
-        const boatSpeed = 50;
+        // Wektor do sprawdzania rotacji łodzi
+        const direction = new Phaser.Math.Vector2(0, 0);
+        direction.setToPolar(this.boat.rotation, 1);
+        const dx = direction.x; //Kierunek rotacji x
+        const dy = direction.y; //Kierunek rotacji y
 
         if(this.keys.left?.isDown){
-            this.boat.angle -= 1;
+            this.boat.angle -= this.boatSpeed / 2;
         }else if(this.keys.right?.isDown){
-            this.boat.angle += 1;
-        }else if(this.keys.up?.isDown){
-                this.boat.setVelocityY(-boatSpeed);
-        }else if(this.keys.down?.isDown){
-            this.boat.setVelocityY(boatSpeed);
-        } else {
-             this.boat.setVelocityX(0);
-             this.boat.setVelocityY(0);
-         }
+            this.boat.angle += this.boatSpeed / 2;
+        }
+        if(this.keys.up?.isDown){
+            // Poruszanie łodzi (Rozpędzanie w czasie)
+            if(this.boatSpeed <= 4){
+                if(this.timer >= 100){
+                    this.boatSpeed += 0.1;
+                    this.boat.y -= this.boatSpeed *dy;
+                    this.boat.x -= this.boatSpeed *dx;
+                    this.timer = 0;
+
+                    console.log(this.boatSpeed)
+                }
+            }
+        }else if(this.keys.up?.isUp){
+            //Utrzymanie prędkości
+            this.engine = true;
+        }
+        if(this.keys.down?.isDown){
+            // Zatrzymywanie łodzi ("zrzucanie kotwicy")
+            if(this.boatSpeed > 0){
+                if(this.timer >= 100){
+                    this.boatSpeed -= 0.25;
+                    this.timer = 0;
+                    if(this.boatSpeed < 0){
+                        this.boatSpeed = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    // funkcja do utrzymywania prędkości łodzi
+    boatEngine(engine, timer){
+        // Wektor do sprawdzania rotacji łódki
+        const direction = new Phaser.Math.Vector2(0, 0);
+        direction.setToPolar(this.boat.rotation, 1);
+        const dx = direction.x; //Kierunek rotacji x
+        const dy = direction.y; //Kierunek rotacji y
+        if (engine = 1){
+            this.boat.y -= this.boatSpeed *dy;
+            this.boat.x -= this.boatSpeed *dx;
+        }
     }
 }
