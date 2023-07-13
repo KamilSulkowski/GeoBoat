859b693e7f1d1adbaa9d0dd74a187860412076fe
Kamil Sulkowski <kamil.sulkowski98@gmail.com>
2023-07-13 14:31:49 +0200

    dodane sledzenie gracza

--- public/src/game.js
+++ public/src/game.js
@@ -9,10 +9,12 @@ export default class Game extends Phaser.Scene {
     create(){
         // Ustawienie tła na niebieskie
         this.cameras.main.setBackgroundColor('#87CEEB');
+        // ustawia wielkość kamery
+        this.cameras.main.setBounds(0,0, 800,600);
 
         // Ustawienie łódki na środek ekranu
         this.boat = this.physics.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, "boat");
-        
+        this.boat2 = this.physics.add.sprite((this.cameras.main.width / 2) + 200, (this.cameras.main.height / 2) + 200, "boat");
         // Zmienna do ustawienia sterowania
         this.keys = this.input.keyboard.createCursorKeys();
 
@@ -20,7 +22,11 @@ export default class Game extends Phaser.Scene {
 
     update(time, delta) {
         super.update(time, delta);
+
         this.moveBoat(this.boat, 10);
+        // kamera śledzi gracza
+        this.cameras.main.startFollow(this.boat);
+
     }
 
     // Funkcja do sterowania łodzią
@@ -28,16 +34,16 @@ export default class Game extends Phaser.Scene {
         const boatSpeed = 50;
 
         if(this.keys.left?.isDown){
-            this.boat.angle += 1;
-        }else if(this.keys.right?.isDown){
             this.boat.angle -= 1;
+        }else if(this.keys.right?.isDown){
+            this.boat.angle += 1;
         }else if(this.keys.up?.isDown){
-                this.boat.setVelocityY(boatSpeed);
+                this.boat.setVelocityY(-boatSpeed);
         }else if(this.keys.down?.isDown){
-            this.boat.setVelocityY(-boatSpeed);}
-        // }else{
-        //     this.boat.setVelocityX(0);
-        //     this.boat.setVelocityY(0);
-        // }
+            this.boat.setVelocityY(boatSpeed);
+        } else {
+             this.boat.setVelocityX(0);
+             this.boat.setVelocityY(0);
+         }
     }
 }
