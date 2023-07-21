2ff8f88217031c105f254c9078a37ef233ceb5ea
Kamil Oglęcki <ok49356@zut.edu.pl>
2023-07-21 11:56:17 +0200

    poprawki kodu quizu

--- public/src/scenes/Quiz.js
+++ public/src/scenes/Quiz.js
@@ -51,15 +51,15 @@ function prepareQuiz(kategoria) {
 
 export function showQuiz(){
 
-    const modalWidth = 800;
-    const modalHeight = 600;
-    const modalX = (this.bw - modalWidth) / 2;
-    const modalY = (this.bh - modalHeight) / 2;
+    this.modalWidth = 800;
+    this.modalHeight = 600;
+    this.modalX = (this.bw - this.modalWidth) / 2;
+    this.modalY = (this.bh - this.modalHeight) / 2;
 
     this.quizOpen = true;
     this.modal = this.add.graphics();
     this.modal.fillStyle(0xffffff, 0.95);
-    this.modal.fillRoundedRect(modalX, modalY, modalWidth, modalHeight, 25);
+    this.modal.fillRoundedRect(this.modalX, this.modalY, this.modalWidth, this.modalHeight, 25);
     setWynik(0, 0, 0, 0);
     fetch('/dane/wynik')
         .then(response => response.text())
@@ -68,16 +68,16 @@ export function showQuiz(){
         });
 
     //Tytuł
-    this.menuText = this.add.text(modalX + modalWidth / 2, modalY + 20, 'Quiz', { fontFamily: 'Arial', fontSize: '24px', fill: '#000000' });
+    this.menuText = this.add.text(this.modalX + this.modalWidth / 2, this.modalY + 20, 'Quiz', { fontFamily: 'Arial', fontSize: '24px', fill: '#000000' });
     this.menuText.setOrigin(0.5);
 
     // Linia oddzielająca
-    const LineSep = new Phaser.Geom.Line(modalX, modalY + 50, modalX + modalWidth, modalY + 50);
+    const LineSep = new Phaser.Geom.Line(this.modalX, this.modalY + 50, this.modalX + this.modalWidth, this.modalY + 50);
     this.modal.lineStyle(2, 0x000000);
     this.modal.strokeLineShape(LineSep);
 
     // Postać co będzie se ruszać ustami jak pytanie będzie lecieć
-    this.quizCharacterImage = this.add.image(modalX + 110, modalY + 150, 'QTPH');
+    this.quizCharacterImage = this.add.image(this.modalX + 110, this.modalY + 150, 'QTPH');
     this.quizCharacterImage.setScale(0.75); // Adjust the scale of the image as needed
 
     //---------------------------------------
@@ -103,7 +103,7 @@ export function showQuiz(){
     //     }
     // })();
 
-    this.aktualnePytanie = 1;
+    this.aktualnePytanie = 0;
     this.punktyZdobyte = 0;
     this.liczbaPytan = 10;
 
@@ -125,51 +125,24 @@ export function showQuiz(){
 
     if (this.dostepnePytania.length < 10)
         this.liczbaPytan = this.dostepnePytania.length;
-    this.QnA = [
-        {
-            question: this.p.tresc,
-            answers: [
-                this.odp[0].tresc,
-                this.odp[1].tresc,
-                this.odp[2].tresc,
-                this.odp[3].tresc,
-            ],
-        },
-        // Tutaj z bazy przypisujemy pytanie do question, odpowiedzi do answers
-    ];
 
-    // Ustawianie zdjęcia
-    if (this.p.obraz !== null) {
-        this.questionImage = this.add.image((this.bw - 800) / 2 + 800 / 2, (this.bh - 600) / 2 + 140, this.p.obraz)
-        this.questionImage.setScale(0.25)
-    }
-
-    let h = 0;
-    this.correctIndex = 0;
-    for (h in this.odp) {
-        if (this.odp[h].czyPoprawna === 1)
-            this.correctIndex = h;
-    }
-    console.log('Correct index:', this.correctIndex);
     drawQuestionAndAnswers.call(this);
 }
 function drawQuestionAndAnswers(){
-    const modalWidth = 800;
-    const modalHeight = 600;
-    const modalX = (this.bw - modalWidth) / 2;
-    const modalY = (this.bh - modalHeight) / 2;
+    // Nowe pytanie
+    newQuestion.call(this);
 
     // Treść pytania
-    const tempQuestion = this.QnA[0]    // <--
+    const tempQuestion = this.QnA[0]
 
     this.quizQuestionTextContent = tempQuestion.question
-    this.quizQuestionText = this.add.text(modalX + modalWidth / 2 + 380, modalY + 235, this.quizQuestionTextContent,
+    this.quizQuestionText = this.add.text(this.modalX + this.modalWidth / 2 + 380, this.modalY + 235, this.quizQuestionTextContent,
         { fontFamily: 'Arial', fontSize: '24px', fill: '#000000', wordWrap: { width: 580, useAdvancedWrap: true }});
     this.quizQuestionText.setOrigin(1);
     this.quizQuestionText.setBackgroundColor('#f0f0f0');
 
     // Linia oddzielająca
-    const LineSep2 = new Phaser.Geom.Line(modalX, modalY + 250, modalX + modalWidth, modalY + 250);
+    const LineSep2 = new Phaser.Geom.Line(this.modalX, this.modalY + 250, this.modalX + this.modalWidth, this.modalY + 250);
     this.modal.lineStyle(2, 0x000000);
     this.modal.strokeLineShape(LineSep2);
 
@@ -187,7 +160,7 @@ function drawQuestionAndAnswers(){
     this.quizAnswerTexts = [];
 
     for (let i = 0; i < answerTexts.length; i++) {
-        this.quizAnswerText = this.add.text(modalX + modalWidth / 2, modalY + yOffset + yOffsetIncrement * i, answerTexts[i], {
+        this.quizAnswerText = this.add.text(this.modalX + this.modalWidth / 2, this.modalY + yOffset + yOffsetIncrement * i, answerTexts[i], {
             fontFamily: 'Arial',
             fontSize: fontSize,
             fill: textColor,
@@ -215,7 +188,7 @@ function drawQuestionAndAnswers(){
     // Liczba pytań
     this.QuestionNumberDisplayedContent = (this.aktualnePytanie) + " / " + (this.liczbaPytan)
     console.log(this.QuestionNumberDisplayedContent)
-    this.QuestionNumberDisplayed = this.add.text(modalX + modalWidth / 2 - 340, modalY + modalHeight - 45, this.QuestionNumberDisplayedContent, {
+    this.QuestionNumberDisplayed = this.add.text(this.modalX + this.modalWidth / 2 - 340, this.modalY + this.modalHeight - 45, this.QuestionNumberDisplayedContent, {
         fontFamily: 'Arial',
         fontSize: '24px',
         fill: '#000000',
@@ -227,7 +200,7 @@ function drawQuestionAndAnswers(){
     this.QuestionNumberDisplayed.setOrigin(0.5);
 
     //Przycisk odpowiedzi
-    this.submitButton = this.add.text(modalX + modalWidth / 2 + 303, modalY + modalHeight - 45, 'Zatwierdź', {
+    this.submitButton = this.add.text(this.modalX + this.modalWidth / 2 + 303, this.modalY + this.modalHeight - 45, 'Zatwierdź', {
         fontFamily: 'Arial',
         fontSize: '24px',
         fill: '#ffffff',
@@ -249,47 +222,13 @@ function drawQuestionAndAnswers(){
             for (this.quizAnswerText of this.quizAnswerTexts) {
                 this.quizAnswerText.destroy();
             }
-            if(this.submitButton){
+            if(this.questionImage)
+                this.questionImage.destroy();
+            if(this.submitButton)
                 this.submitButton.destroy();
-            }
             this.QuestionNumberDisplayed.destroy();
 
-            // Nowe pytanie i nowe odpowiedzi
-            this.p = this.pytania.find((row) => row.id === this.dostepnePytania[this.aktualnePytanie]);
-            console.log(this.p);
-            this.aktualnePytanie += 1;
-            this.odp = [];
-            for (let i = 0; i < this.dostepneOdpowiedzi.length; i++) {
-                if (this.o[i].idPytania === this.p.id)
-                    this.odp.push(this.o[i]);
-            }
-
-            this.QnA = [
-                {
-                    question: this.p.tresc,
-                    answers: [
-                        this.odp[0].tresc,
-                        this.odp[1].tresc,
-                        this.odp[2].tresc,
-                        this.odp[3].tresc,
-                    ],
-                },
-            ];
-
-            if (this.p.obraz !== null) {
-                this.questionImage = this.add.image((this.bw - 800) / 2 + 800 / 2, (this.bh - 600) / 2 + 140, this.p.obraz)
-                this.questionImage.setScale(0.25)
-            }
-
-            let h = 0;
-            this.correctIndex = 0;
-            for (h in this.odp) {
-                if (this.odp[h].czyPoprawna === 1)
-                    this.correctIndex = h;
-            }
-            console.log('Correct index:', this.correctIndex);
             drawQuestionAndAnswers.call(this);
-
         }
         else if(this.aktualnePytanie === this.liczbaPytan){
 
@@ -304,7 +243,8 @@ function drawQuestionAndAnswers(){
                 this.menuText.destroy();
                 this.quizCharacterImage.destroy();
                 this.quizQuestionText.destroy();
-                this.questionImage.destroy();
+                if(this.questionImage)
+                    this.questionImage.destroy();
                 for (this.quizAnswerText of this.quizAnswerTexts) {
                     this.quizAnswerText.destroy();
                 }
@@ -316,27 +256,61 @@ function drawQuestionAndAnswers(){
             showEndScreen.call(this);
         }
         console.log('Selected answer index:', this.selectedAnswerIndex);
-        console.log('Punkty zdobyte!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ', this.punktyZdobyte);
+        console.log('Punkty zdobyte: ', this.punktyZdobyte);
     });
 }
 
-function showEndScreen() {
-    const modalWidth = 800;
-    const modalHeight = 600;
-    const modalX = (this.bw - modalWidth) / 2;
-    const modalY = (this.bh - modalHeight) / 2;
+function newQuestion() {
+    // Nowe pytanie i nowe odpowiedzi
+    this.p = this.pytania.find((row) => row.id === this.dostepnePytania[this.aktualnePytanie]);
+    console.log(this.p);
+    this.aktualnePytanie += 1;
+    this.odp = [];
+    for (let i = 0; i < this.dostepneOdpowiedzi.length; i++) {
+        if (this.o[i].idPytania === this.p.id)
+            this.odp.push(this.o[i]);
+    }
 
+    this.QnA = [
+        {
+            question: this.p.tresc,
+            answers: [
+                this.odp[0].tresc,
+                this.odp[1].tresc,
+                this.odp[2].tresc,
+                this.odp[3].tresc,
+            ],
+        },
+        // Przypisujemy pytanie do question, odpowiedzi do answers
+    ];
+
+    // Ustawianie zdjęcia
+    if (this.p.obraz !== null) {
+        this.questionImage = this.add.image(this.modalX + this.modalWidth / 2, this.modalY + 140, this.p.obraz)
+        this.questionImage.setScale(0.25)
+    }
+
+    // Ustawienie indeksu poprawnej odpowiedzi
+    let h = 0;
+    this.correctIndex = 0;
+    for (h in this.odp) {
+        if (this.odp[h].czyPoprawna === 1)
+            this.correctIndex = h;
+    }
+    console.log('Correct index:', this.correctIndex);
+}
+function showEndScreen() {
     // Gratulacje
     let header = 'Gratulacje, quiz ukończony'
 
-    this.header = this.add.text(modalWidth / 2, modalY + 110, header,
+    this.quizQuestionText = this.add.text(this.modalWidth / 2, this.modalY + 110, header,
         { fontFamily: 'Arial', fontSize: '50px', fill: '#000000', wordWrap: { width: 780, useAdvancedWrap: true }});
 
     this.submitButton.visible = true;
 
     // Wynik
     let points = "Wynik: \n " + (this.punktyZdobyte) + " / " + (this.liczbaPytan)
-    this.points = this.add.text(modalX + modalWidth / 2 - 100, modalHeight - 65, points, {
+    this.quizAnswerText = this.add.text(this.modalX + this.modalWidth / 2 - 100, this.modalHeight - 65, points, {
         fontFamily: 'Arial',
         fontSize: '50px',
         fill: '#000000',
@@ -345,9 +319,10 @@ function showEndScreen() {
             y: 10,
         },
     });
+    this.quizAnswerTexts.push(this.quizAnswerText);
 
     //Zakończ
-    this.submitButton = this.add.text(modalX + modalWidth / 2, modalY + modalHeight - 45, 'Zakończ', {
+    this.submitButton = this.add.text(this.modalX + this.modalWidth / 2, this.modalY + this.modalHeight - 45, 'Zakończ', {
         fontFamily: 'Arial',
         fontSize: '40px',
         fill: '#ffffff',
@@ -361,7 +336,6 @@ function showEndScreen() {
     this.submitButton.setInteractive({ useHandCursor: true });
 
     this.submitButton.on('pointerdown', () => {
-        //updateDatabase.call(this);
         this.quizQuestionText.destroy();
         for (this.quizAnswerText of this.quizAnswerTexts) {
             this.quizAnswerText.destroy();
@@ -382,11 +356,11 @@ function showEndScreen() {
         this.modal.clear();
         if (this.menuText) {
             this.menuText.destroy();
-            this.header.destroy();
+            this.quizQuestionText.destroy();
             if(this.submitButton){
                 this.submitButton.destroy();
             }
-            this.points.destroy();
+            this.quizAnswerText.destroy();
             this.quizOpen = false;
         }
 
@@ -431,10 +405,14 @@ export function closeQuiz(){
             for (this.quizAnswerText of this.quizAnswerTexts) {
                 this.quizAnswerText.destroy();
             }
+            if(this.questionImage)
+                this.questionImage.destroy();
             if(this.submitButton){
                 this.submitButton.destroy();
             }
             this.QuestionNumberDisplayed.destroy();
+            this.quizCharacterImage.destroy();
+            this.quizQuestionText.destroy();
         }
         this.quizOpen = false;
         // this.quizAnswerButtons = [];
